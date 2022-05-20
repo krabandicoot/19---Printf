ed1b81efe473c1b3b2a541b37982a0847ebe04b5
Khadja Paux <khadjapaux.contact@gmail.com>
2022-05-20 16:47:58 +0200

    Base with notes and libft changed

---
+++ includes/libft.h
@@ -0,0 +1,15 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+
+void	ft_putchar(char c);
+void	ft_putnbr(int n);
+void	ft_putstr(char *s);
+size_t	ft_strlen(const char *s);
+
+
+#endif
